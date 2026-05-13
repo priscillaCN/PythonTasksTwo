3b860a42a608bfbe154f0539b2a52d7bf9c7e6b8
Priscilla <priscillannamani00@gmail.com>
2026-05-13 09:44:51 +0100

    Create exercise_two.py

---
+++ Assignment/Dietel chapter 4/exercise_two.py
@@ -0,0 +1,6 @@
+"""
+If you keep pushing onto a stack without popping enough, you'll eventually run out of memory. 
+In the context of the function-call stack, this causes a fatal error called a stack overflow discussed in section 4.16. 
+"If the function-call stack runs out of memory as a result of too many function calls, a fatal error known as stack overflow occurs. 
+Stack overflows actually are rare unless you have a logic error that keeps calling functions that never return." 
+"""
